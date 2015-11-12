6e40e60412218df5af66df555d85cf6e291626ed
Pascal Grueter <pas.grueter@gmail.com>
2015-11-12 14:48:34 +0100

    model classes with hibernate

---
+++ src/model/User_Game.java
@@ -0,0 +1,49 @@
+package model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user_game")
+public class User_Game {
+
+    private int id;
+    private User user;
+    private Game game;
+
+    public User_Game (){
+
+    }
+
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "game_id")
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+}
