82cee2f362fa343227eb260e466f4567dd1ae352
Pascal Grueter <pas.grueter@gmail.com>
2015-12-17 14:36:06 +0100

    propertiesUtil changed

--- src/bookmaker/PropertiesUtil.java
+++ src/bookmaker/PropertiesUtil.java
@@ -3,37 +3,46 @@ package bookmaker;
 import model.Condition;
 
 import java.text.Format;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
  * Utility class for fetching the appropriate text for conditions and teams from the properties file.
  */
 public final class PropertiesUtil {
 
-    private static ResourceBundle bundle;
-    private static String[] conditions, teams;
+    private static ResourceBundle langBundle, condBundle, teamBundle;
+    private static Map<String, String> conditions, teams;
 
     private PropertiesUtil(){
 
     }
 
-    public enum Type {
-            LEAD_TIME,
-            LEAD_AMOUNT,
-            LEAD_TIME_AMOUNT;
-
-    };
 
     /**
-     * Reads the properties file for the corresponding language and saves the conditions and teams in arrays
+     * Reads the properties file for the corresponding language and saves the conditions and teams in maps
      * Needs to be called once for proper initialization and each time the language changes
      * @param language the desired language
      */
     public static void readProperties(String language){
-        bundle = ResourceBundle.getBundle("lang", new Locale(language));
-        conditions = bundle.getStringArray("conditions");
-        teams = bundle.getStringArray("teams");
+        langBundle = ResourceBundle.getBundle("lang", new Locale(language));
+        condBundle = ResourceBundle.getBundle("conditions", new Locale(language));
+        teamBundle = ResourceBundle.getBundle("teams", new Locale(language));
+
+        conditions = bundleToMap(condBundle);
+        teams = bundleToMap(teamBundle);
+    }
+
+    private static Map<String, String> bundleToMap(ResourceBundle bundle){
+        // get the keys
+        Enumeration<String> enumeration = bundle.getKeys();
+        Map<String, String> tempMap = new HashMap<String, String>();
+        String key = "";
+        while (enumeration.hasMoreElements()) {
+            key = enumeration.nextElement();
+            tempMap.put(key,bundle.getString(key));
+        }
+
+        return tempMap;
     }
 
     /**
@@ -41,8 +50,8 @@ public final class PropertiesUtil {
      * @param id condition-ID
      * @return condition text in String
      */
-    public static String getConditionPerId(int id){
-        return conditions[id];
+    public static String getConditionPerId(String id){
+        return conditions.get(id);
     }
 
     /**
@@ -51,31 +60,20 @@ public final class PropertiesUtil {
      * @param leadingTeam the leading team
      * @return condition text in String
      */
-    public static String getConditionPerId(int id, String leadingTeam){
-        return String.format(conditions[id], leadingTeam);
+    public static String getConditionPerId(String id, String leadingTeam){
+        return String.format(conditions.get(id), leadingTeam);
     }
 
     /**
      * For the situations where one team leads at a specific time and/or amount of goals
      * Gets the appropriate condition text with variables for the corresponding id
-     * Possible type values are: LEAD_TIME, LEAD_AMOUNT, LEAD_TIME_AMOUNT
-     * LEAD_TIME and LEAD_AMOUNT accept one param and LEAD_TIME_AMOUNT needs first the time and second the amount
      * @param id condition-ID
      * @param leadingTeam the leading team
      * @param params if type is LEAD_TIME_AMOUNT, time and amount are needed, else only one int
-     * @param type see above
      * @return condition text in String
      */
-    public static String getConditionPerId(int id, String leadingTeam, int[] params, Type type){
-        if (type == Type.LEAD_TIME || type == Type.LEAD_AMOUNT){
-            return String.format(conditions[id], leadingTeam, params[0]);
-        }
-        else if(type == Type.LEAD_TIME_AMOUNT){
-            return String.format(conditions[id], leadingTeam, params[0], params[1]);
-        }
-        else{
-            return "No such condition";
-        }
+    public static String getConditionPerId(String id, String leadingTeam, int[] params){
+        return String.format(conditions.get(id), leadingTeam, params);
     }
 
     /**
@@ -83,17 +81,26 @@ public final class PropertiesUtil {
      * @param id team-ID
      * @return Teamname in String
      */
-    public static String getTeamPerId(int id){
-        return teams[id];
+    public static String getTeamPerId(String id){
+        return teams.get(id);
     }
 
     /**
      * Gets a list of all teams within the properties file
      * Used for rendering the team select options
-     * @return Stringarray with all existing teams
+     * @return Map<String, String>
      */
-    public static String[] getTeamList(){
+    public static Map<String, String> getTeamList(){
         return teams;
+    }
+
+    /**
+     * Gets a list of all conditions within the properties file
+     * Used for rendering the condition select options
+     * @return Map<String, String>
+     */
+    public static Map<String, String> getConditionList(){
+        return conditions;
     }
 
 }
