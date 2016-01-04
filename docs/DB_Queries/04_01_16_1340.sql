3ed4e5520515ef98820e18274c922bf22166b142
Stefan <andos1@bfh.ch>
2016-01-04 13:59:16 +0100

    added column closed to game

--- docs/DB_Queries/04_01_16_1340.sql
+++ docs/DB_Queries/04_01_16_1340.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `bookmaker`.`condition`
 ADD COLUMN `occurred` BIT(1) NULL DEFAULT NULL COMMENT '' AFTER `odd`;
+
+ALTER TABLE `bookmaker`.`game` 
+ADD COLUMN `closed` BIT(1) NULL DEFAULT FALSE COMMENT '' AFTER `owner`;
